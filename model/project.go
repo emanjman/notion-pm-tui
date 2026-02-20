241b3a3d8e4a8347cfcfe68a1cd6a71b79091045
Emanuel Pecson <emanpecson@gmail.com>
2026-02-19 18:21:29 -0800

    feat: :sparkles: setup initial project model

---
+++ model/project.go
@@ -0,0 +1,19 @@
+package model
+
+type Tab int
+
+// enum representation of each
+const (
+	MilestonesTab Tab = iota
+	OverviewTab
+	ProjectNotesTab
+	DebugNotesTab
+)
+
+type ProjectModel struct {
+	activeTab    Tab
+	milestones   views.MilestonesListModel
+	overview     views.PageContentModel
+	projectNotes views.NotesListModel
+	debugNotes   views.NotesListMode
+}
