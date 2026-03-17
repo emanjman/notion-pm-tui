f91f15b089e2357b593d45fa55a39aa0c80d1837
Emanuel Pecson <emanpecson@gmail.com>
2026-03-16 18:00:05 -0700

    feat: :sparkles: wrap up init note item

---
+++ components/list/note/item.go
@@ -0,0 +1,24 @@
+package note
+
+import "notion-project-tui/notion"
+
+type Item struct {
+	ID           string
+	Title        string
+	CreatedLabel string
+}
+
+func (x Item) FilterValue() string { return x.Title }
+
+func NewItem(page notion.NotePage) Item {
+	createdLabel := ""
+	if page.Properties.CreatedLabel.Formula.String != nil {
+		createdLabel = *page.Properties.CreatedLabel.Formula.String
+	}
+
+	return Item{
+		ID:           page.ID,
+		Title:        notion.ExtractPlainText(page.Properties.Title.Title),
+		CreatedLabel: createdLabel,
+	}
+}
