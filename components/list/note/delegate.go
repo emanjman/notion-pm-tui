a540b86603e346f77e6be35de93ba2eb88563f25
Emanuel Pecson <emanpecson@gmail.com>
2026-03-17 15:58:20 -0700

    style: :lipstick: stylize note list

--- components/list/note/delegate.go
+++ components/list/note/delegate.go
@@ -3,31 +3,87 @@ package note
 import (
 	"fmt"
 	"io"
+	"notion-project-tui/styles"
 
 	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
+	lg "github.com/charmbracelet/lipgloss"
 )
 
-type ItemDelegate struct {
-	focused bool
+type variantStyle struct {
+	base    lg.Style
+	focused lg.Style
 }
 
-func NewItemDelegate(focused bool) ItemDelegate {
+type style struct {
+	itemContainer variantStyle
+	itemSegment   variantStyle
+}
+
+type ItemDelegate struct {
+	sectionFocused bool
+	style          style
+}
+
+func NewItemDelegate(sectionFocused bool) ItemDelegate {
+	borderDistance := 0
+	rightEdgeDistance := 3
+
+	var (
+		contBase = lg.NewStyle().
+				Border(lg.NormalBorder(), false, false, true, false).
+				BorderForeground(styles.BorderForeground).
+				PaddingLeft(borderDistance + 2).
+				PaddingRight(rightEdgeDistance)
+		contFocus = contBase.
+				Background(styles.SelectedBackground)
+	)
+
+	var (
+		segBase  = lg.NewStyle()
+		segFocus = segBase.
+				Background(styles.SelectedBackground)
+	)
+
 	return ItemDelegate{
-		focused: focused,
+		sectionFocused: sectionFocused,
+		style: style{
+			itemContainer: variantStyle{base: contBase, focused: contFocus},
+			itemSegment:   variantStyle{base: segBase, focused: segFocus},
+		},
 	}
 }
 
-func (d ItemDelegate) Height() int                               { return 1 }
+func (d ItemDelegate) Height() int                               { return 2 }
 func (d ItemDelegate) Spacing() int                              { return 0 }
 func (d ItemDelegate) Update(msg tea.Msg, m *list.Model) tea.Cmd { return nil }
 
 func (d ItemDelegate) Render(w io.Writer, m list.Model, index int, item list.Item) {
-	// todo: implement hover styling
+	focused := index == m.Index() && d.sectionFocused
+
 	switch item := item.(type) {
 	case Item:
-		fmt.Fprint(w, item.Title)
-	default:
-		fmt.Fprint(w, "Unhandled")
+		contStyle := d.style.itemContainer.base
+		segStyle := d.style.itemSegment.base
+		titleStyle, dateStyle := lg.Style{}, lg.Style{}
+
+		if focused {
+			segStyle = d.style.itemSegment.focused
+			contStyle = d.style.itemContainer.focused
+			titleStyle, dateStyle = segStyle, segStyle
+		}
+
+		// apply field-specific styles
+		titleStyle = titleStyle.Foreground(styles.PrimaryForeground)
+		dateStyle = dateStyle.Foreground(styles.MutedForeground)
+
+		// render each field
+		title := titleStyle.Render(item.Title)
+		created := dateStyle.Render(item.CreatedLabel)
+
+		px := styles.GetPaddingBetween(title, created, m.Width(), contStyle)
+		content := title + styles.RenderPadding(segStyle, px) + created
+
+		fmt.Fprint(w, contStyle.Width(m.Width()).Render(content))
 	}
 }
