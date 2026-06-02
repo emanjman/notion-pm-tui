ec8729d7d6598f3f84742d300e382bb90e39545e
Emanuel Pecson <emanpecson@gmail.com>
2026-06-02 12:17:06 -0700

    chore: 🚚 follow up w/ renamed props in @project-milestones

--- notion/milestone_props.go
+++ notion/milestone_props.go
@@ -1,11 +1,18 @@
 package notion
 
+type MilestoneProperties struct {
+	Title     TitleProperty   `json:"name"`
+	Progress  FormulaProperty `json:"$progress"` // type:number
+	Status    FormulaProperty `json:"$status"`   // type:string
+	TaskCount FormulaProperty `json:"$task-ct"`  // type:number
+}
+
 var (
 	milestonePropTitle                 = "name"
-	milestonePropProgress              = "progress"
+	milestonePropProgress              = "$progress"
 	milestonePropVersionRelation       = "@version"
 	milestonePropProjectRollupRelation = "r/@project"
 	milestonePropStatusFormula         = "$status"
 	milestonePropLatestActivityAt      = "$latest-activity-at"
-	milestonePropTaskCount             = "task-ct"
+	milestonePropTaskCount             = "$task-ct"
 )
